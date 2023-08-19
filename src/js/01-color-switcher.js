7bd9632b3cb04f1931e44c7cee9c637d52c28c94
YuraTataryn <127976814+YuraTataryn@users.noreply.github.com>
2023-08-19 09:24:03 -0700

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,27 +1,26 @@
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
-  }
-  
-  const startButton = document.querySelector('[data-start]');
-  const stopButton = document.querySelector('[data-stop]');
-  const body = document.body;
-  
-  let intervalId = null;
-  
-  startButton.addEventListener('click', () => {
-    startButton.disabled = true;
-    stopButton.disabled = false;
-  
-    intervalId = setInterval(() => {
-      const randomColor = getRandomHexColor();
-      body.style.backgroundColor = randomColor;
-    }, 1000);
-  });
-  
-  stopButton.addEventListener('click', () => {
-    startButton.disabled = false;
-    stopButton.disabled = true;
-  
-    clearInterval(intervalId);
-  });
-  
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.body;
+
+let intervalId = null;
+
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+
+  intervalId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    body.style.backgroundColor = randomColor;
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+
+  clearInterval(intervalId);
+});
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,100 +1,86 @@
+
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      const selectedDate = selectedDates[0];
-      const currentDate = new Date();
-      
-      if (selectedDate <= currentDate) {
-        window.alert("Please choose a date in the future");
-        return;
-      }
-      
-      document.getElementById("start-button").removeAttribute("disabled");
-    },
-  };
-  
-  const dateTimePicker = flatpickr("#datetime-picker", options);
-  
-  const startButton = document.getElementById("start-button");
-  startButton.addEventListener("click", startCountdown);
-  
-  let countdownInterval;
-  
-  function startCountdown() {
-    const selectedDate = dateTimePicker.selectedDates[0];
-    const now = new Date();
-    
-    if (selectedDate <= now) {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const currentDate = new Date();
+
+    if (selectedDate <= currentDate) {
+      window.alert("Please choose a date in the future");
       return;
     }
-  
-    startButton.setAttribute("disabled", true);
-  
-    countdownInterval = setInterval(updateCountdown, 1000, selectedDate);
+
+    startButton.removeAttribute("disabled");
+  },
+};
+
+const dateTimePicker = flatpickr("#datetime-picker", options);
+
+const startButton = document.querySelector("[data-start]");
+const timerFields = document.querySelectorAll(".value");
+const dateTimeInput = dateTimePicker._input;
+let countdownInterval;
+
+startButton.addEventListener("click", startCountdown);
+
+function startCountdown() {
+  const selectedDate = dateTimePicker.selectedDates[0];
+  const now = new Date();
+
+  if (selectedDate <= now) {
+    return;
   }
-  
-  function updateCountdown(targetDate) {
-    const timeDifference = targetDate - new Date();
-    
-    if (timeDifference <= 0) {
-      clearInterval(countdownInterval);
-      updateTimerDisplay(0, 0, 0, 0);
-      return;
-    }
-    
-    const timeComponents = convertMs(timeDifference);
-    updateTimerDisplay(
-      timeComponents.days,
-      timeComponents.hours,
-      timeComponents.minutes,
-      timeComponents.seconds
-    );
+
+  startButton.setAttribute("disabled", true);
+  dateTimeInput.setAttribute("disabled", true);
+
+  countdownInterval = setInterval(updateCountdown, 1000, selectedDate);
+}
+
+function updateCountdown(targetDate) {
+  const timeDifference = targetDate - new Date();
+
+  if (timeDifference <= 0) {
+    clearInterval(countdownInterval);
+    updateTimerDisplay(0, 0, 0, 0);
+    dateTimeInput.removeAttribute("disabled");
+    startButton.removeAttribute("disabled");
+    return;
   }
-  
-  function convertMs(ms) {
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    const days = Math.floor(ms / day);
-    const hours = Math.floor((ms % day) / hour);
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
-  
-  function updateTimerDisplay(days, hours, minutes, seconds) {
-    document.querySelector('[data-days]').textContent = addLeadingZero(days);
-    document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
-    document.querySelector('[data-minutes]').textContent = addLeadingZero(minutes);
-    document.querySelector('[data-seconds]').textContent = addLeadingZero(seconds);
-  }
-  
-  function addLeadingZero(value) {
-    return value.toString().padStart(2, '0');
-  }
-  
 
+  const timeComponents = convertMs(timeDifference);
+  updateTimerDisplay(
+    timeComponents.days,
+    timeComponents.hours,
+    timeComponents.minutes,
+    timeComponents.seconds
+  );
+}
 
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
+  return { days, hours, minutes, seconds };
+}
 
+function updateTimerDisplay(days, hours, minutes, seconds) {
+  timerFields[0].textContent = addLeadingZero(days);
+  timerFields[1].textContent = addLeadingZero(hours);
+  timerFields[2].textContent = addLeadingZero(minutes);
+  timerFields[3].textContent = addLeadingZero(seconds);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+function addLeadingZero(value) {
+  return value.toString().padStart(2, "0");
+}
